09114716939f127e768670d93fbe7c3b3dc7c4eb
Joppe Hoekstra <joppe.hoekstra@milieudefensie.nl>
2023-01-26 12:17:56 +0100

    changes

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,9 +5,9 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '1', component: () => import('pages/Version1.vue') },
-      { path: '2', component: () => import('pages/Version2.vue') },
-      { path: '3', component: () => import('pages/Version3.vue') }
+      { path: '1', component: () => import('pages/VersionOne.vue') },
+      { path: '2', component: () => import('pages/VersionTwo.vue') },
+      { path: '3', component: () => import('pages/VersionThree.vue') }
     ]
   },
 
